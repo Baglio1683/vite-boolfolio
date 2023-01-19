bc2bf6abe3295cb6120c9a87d46434697934f159
MyUsername <alberto.baglivi@gmail.com>
2023-01-19 17:12:03 +0100

    Add the AppHeader and AppMain

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,24 @@
+<script>
+
+export default {
+    name: 'AppHeader',
+    components: {
+
+    }
+}
+
+</script>
+
+<template>
+    <header>
+        <div class="container">
+            <div class="d-flex justify-content-center">
+                <h1>PROGETTI</h1>
+            </div>
+        </div>
+    </header>
+</template>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,24 @@
+<script>
+
+export default {
+    name: 'AppMain',
+    components: {
+
+    }
+}
+
+</script>
+
+<template>
+    <main>
+        <div class="container">
+            <div class="row">
+
+            </div>
+        </div>
+    </main>
+</template>
+
+<style lang="scss">
+
+</style>
