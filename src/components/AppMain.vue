650e41f3f47a48b552aa75f096945a9f0779d7d8
MyUsername <alberto.baglivi@gmail.com>
2023-01-19 17:56:31 +0100

    print the titles of projects

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,22 +1,51 @@
 <script>
+import axios from 'axios';
 
 export default {
     name: 'AppMain',
-    components: {
 
-    }
+    data() {
+        return {
+            baseUrl: "http://127.0.0.1:8000",
+            projects: [],
+            loading: false
+        };
+    },
+
+
+    created() {
+        this.getProjects();
+    },
+
+    methods: {
+        getProjects() {
+            this.loading = true;
+            axios.get(`${this.baseUrl}/api/user`).then(resp => {
+                this.projects = resp.data.results;
+                console.log(this.projects);
+                this.loading = false;
+            });
+        }
+    },
+
 }
 
 </script>
 
 <template>
+
     <main>
         <div class="container">
             <div class="row">
-
+                <ul>
+                    <li v-for="project in projects">
+                        {{ project.title }}
+                    </li>
+                </ul>
             </div>
         </div>
     </main>
+
 </template>
 
 <style lang="scss">
